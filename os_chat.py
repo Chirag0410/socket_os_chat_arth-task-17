621cfc5766c9d36d85db0d0d2402a94e53c4d41e
Chirag Goyal <55623370+Chirag0410@users.noreply.github.com>
2021-02-06 14:00:15 +0530

    Create os_chat.py

---
+++ os_chat.py
@@ -0,0 +1,23 @@
+import socket
+import os
+import time
+
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("!!!!!!!--------!!!!!!!!!----------------!!!!!!!!!------------------!!!!!!!\n")
+print("\t\tChat Application without multithreading")
+print("!!!!!!!--------!!!!!!!!!----------------!!!!!!!!!------------------!!!!!!!\n")
+print("hey!!!!..... This is chirag os\n")            
+ip=input("Enter your IP: ")
+port=int(input("Enter your port number: "))
+send_ip=input("Enter sender IP: ")
+send_port=int(input("Enter sender port number: "))
+s.bind((ip,port))
+while True:
+  i=input("send message:")
+  s.sendto(i.encode(), (send_ip,send_port))
+  x=s.recvfrom(20)
+  clientip=x[1][0]
+
+  data=x[0].decode()
+
+  print('\t\t\t\t\t recv message' + clientip + " : " + data)
